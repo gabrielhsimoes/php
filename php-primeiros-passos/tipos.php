608620909d0ea75b67c4949c8dca7bf3b2c4cc67
Gabriel Henrique <gabriel.henrique@evereste.org.br>
2023-03-10 14:33:38 -0400

    tipos de dados

---
+++ php-primeiros-passos/tipos.php
@@ -0,0 +1,17 @@
+<?php
+
+$idade = 20; 
+
+$salario = 1000.30;
+$divisao = 10 / 3; //valor do tipo float, double
+
+$divisao = 3; //valor do tipo inteiro
+
+$texto = "Olá mundo";
+
+$verdadeiro = true;
+$falso = false;
+
+//echo gettype($divisao); // 'gettype' utilizado para identificar o tipo de valor da variável
+
+echo gettype($falso);
