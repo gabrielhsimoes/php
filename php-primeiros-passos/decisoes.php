7cde8d2691b063ff2c696e91d5b6db0fa28dc69c
Gabriel Henrique <gabriel.henrique@evereste.org.br>
2023-03-21 13:25:21 -0400

    decisoes condicionais com if e else

--- php-primeiros-passos/decisoes.php
+++ php-primeiros-passos/decisoes.php
@@ -1,15 +1,22 @@
 <?php
 
-$idade = 19;
+$idade = 16;
+$numeroDePessoas = 1;
 // $nome = 'Gabriel';
 
-echo "Você só pode entrar se tiver a partir de 18 anos." . PHP_EOL; //Quebra de linha
-
+echo "Você só pode entrar se tiver a partir de 18 anos ou" . PHP_EOL;
+echo "a partir de 16 anos acompanhado" . PHP_EOL . PHP_EOL;
 
 if ($idade >= 18){ //$nome == 'Gabriel'
     echo "Você tem $idade anos." . PHP_EOL;
     echo 'Pode entrar!';
+} else if ($idade >= 16 && $numeroDePessoas > 1){
+
+    echo "Você tem $idade anos, mas está acompanhado(a). Pode entrar!";
+} else{
+
+    echo "Você só tem $idade anos, você não pode entrar!";
 }
 
-
-echo "Você só tem $idade anos, você não pode entrar!";
+echo PHP_EOL;
+echo "Adeus!";
