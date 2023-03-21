33407753e0118b0dfbb0523172cc097d68ffeb70
Gabriel Henrique <gabriel.henrique@evereste.org.br>
2023-03-21 08:56:01 -0400

    tomada de decisoes

--- php-primeiros-passos/decisoes.php
+++ php-primeiros-passos/decisoes.php
@@ -1,11 +1,15 @@
 <?php
 
 $idade = 19;
+// $nome = 'Gabriel';
 
 echo "Você só pode entrar se tiver a partir de 18 anos." . PHP_EOL; //Quebra de linha
 
 
-if ($idade >= 18){
+if ($idade >= 18){ //$nome == 'Gabriel'
     echo "Você tem $idade anos." . PHP_EOL;
     echo 'Pode entrar!';
 }
+
+
+echo "Você só tem $idade anos, você não pode entrar!";
