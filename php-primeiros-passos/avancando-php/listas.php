3f9737c2ddae1ce6e1381d4d354e779e02a6cea7
Gabriel Henrique <gabriel.henrique@evereste.org.br>
2023-03-27 15:25:29 -0400

    trabalhando com loops e arrays

---
+++ php-primeiros-passos/avancando-php/listas.php
@@ -0,0 +1,6 @@
+<?php
+
+$idadeList = [21, 23, 19, 30, 40]; 
+$umaIdade = $idadeList[0];
+
+echo $umaIdade;
---
+++ php-primeiros-passos/avancando-php/loop-lista.php
@@ -0,0 +1,9 @@
+<?php
+
+$idadeList = [21, 23, 19, 30, 40, 28, 34, 1];
+
+
+for($i=0; $i < count($idadeList); $i++){
+
+    echo $idadeList[$i] . PHP_EOL;
+}
